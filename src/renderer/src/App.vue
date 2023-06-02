e3d8ddca1eaa6eed55a628b0a71a541564c2f793
Kutay Çelebi <celebikutay@gmail.com>
2023-06-02 19:00:52 +0300

    add transition between route

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -6,18 +6,26 @@ import IconoirAlarm from '~icons/iconoir/alarm'
 <template>
   <el-container>
     <el-aside width="65px" class="sidebar my2">
-      <el-menu mode="vertical" class="my-2 side-menu" collapse router>
-        <el-menu-item :route="{ path: '/timesheet' }" index="timesheet">
+      <el-menu mode="vertical" class="my-2 side-menu" collapse router :default-active="$route.path">
+        <el-menu-item index="/timesheet">
           <iconoir-alarm />
         </el-menu-item>
-        <el-menu-item :route="{ path: '/todo' }" index="todo">
+        <el-menu-item index="/todo">
           <iconoir-check-circle />
         </el-menu-item>
       </el-menu>
     </el-aside>
     <el-container>
       <el-main>
-        <router-view />
+        <router-view v-slot="{ Component, route }">
+          <!--          <template #default="component">-->
+          <transition name="slide">
+            <div :key="route.path">
+              <component :is="Component" />
+            </div>
+          </transition>
+          <!--          </template>-->
+        </router-view>
       </el-main>
     </el-container>
   </el-container>
@@ -41,5 +49,24 @@ import IconoirAlarm from '~icons/iconoir/alarm'
 
 .sidebar {
   padding: 20px 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.3s ease;
+}
+
+.slide-leave-from,
+.slide-enter-to {
+  opacity: 1;
+  position: relative;
+  z-index: 1;
+}
+
+.slide-leave-to,
+.slide-enter-from {
+  opacity: 0;
+  position: absolute;
+  z-index: 0;
 }
 </style>
