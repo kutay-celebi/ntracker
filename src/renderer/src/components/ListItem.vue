cb1ce9589de50cdab73ee9866861e1616e015c7c
Kutay Çelebi <celebikutay@gmail.com>
2023-06-02 22:43:01 +0300

    initialize list component

---
+++ src/renderer/src/components/ListItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="list-item">
+    <slot name="prefix" />
+    <slot />
+    <slot name="suffix" />
+  </div>
+</template>
+
+<style scoped lang="less">
+.list-item {
+  display: flex;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: var(--el-border-width) var(--el-border-style) var(--el-border-color);
+}
+</style>
